2707cd22ebc2af6591c72e16ebe1b11974347018
nikhil <nkmittal4994@gmail.com>
2015-01-25 19:45:27 +0530

    tweaks done for ubuntu.

---
+++ ubuntu/tweak.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install python-pip
+sudo apt-get install python3-pip
+sudo pip install virtualenv
+sudo pip3 install virtualenv
