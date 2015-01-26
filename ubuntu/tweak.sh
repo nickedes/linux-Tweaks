53faacecd7013a997855d6b14e2f77d2f1cb55e6
nikhil <nkmittal4994@gmail.com>
2015-01-26 13:08:43 +0530

    added sublime text 3(beta)

--- ubuntu/tweak.sh
+++ ubuntu/tweak.sh
@@ -7,3 +7,10 @@ sudo apt-get install python-pip
 sudo apt-get install python3-pip
 sudo pip install virtualenv
 sudo pip3 install virtualenv
+
+[sublime-text-3](http://www.webupd8.org/2013/07/sublime-text-3-ubuntu-ppa-now-available.html)
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+sudo apt-get install shutter
